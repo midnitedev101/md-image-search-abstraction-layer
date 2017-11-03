7fda99d5c611e086184f59e2486f14f2e11bbbef
Glitch (md-image-search-abstraction-layer) <none>
2017-11-03 21:05:46 +0000

    Checkpoint

--- server_editable.js
+++ server_editable.js
@@ -67,34 +67,34 @@ client.search('lolcats')
 //client.search('Steve Angello', {size: 'large'});
 });
 
-app.use('/request-test', function(req, res) {
+app.use('/:search_val', function(req, res) {
   var objectArr = [];
   
-  // request('https://www.googleapis.com/customsearch/v1?q=grumpy%20cat&cx='+process.env.CSE_ID+'&num=10&searchType=image&key='+process.env.API_KEY+'', function (error, response, body) {
-  request('https://www.googleapis.com/customsearch/v1?q=lol+cat&cx=015345702908136904402%3A5qvmxdsto6w&searchType=image&key=AIzaSyA1XQ5N5xZsFyRc0OmoMJPhNMczYvXWEso', function (error, response, body) {
-      if (!error && response.statusCode == 200) {
-          // console.log(JSON.parse(body));                               // Returns body of request test
-          var results = JSON.parse(body);
-          // console.log((results);
-          console.log((results.items).length);
-          for(var i = 0; i < (results.items).length; i++) {
-            var cseObject = {'Image URL' : results.items[i].link, 'Context' : results.items[i].link, 'Snippet' : results.items[i].snippet, 'Thumbnail' : results.items[i].image.thumbnailLink};
-            // console.log(results.items[i]);
-            console.log('Context: ' +results.items[i].link);
-            console.log('Snippet: ' +results.items[i].snippet);
-            console.log('Thumbnail: ' +results.items[i].image.thumbnailLink);
-            console.log('Image URL: ' +results.items[i].link);
+//   request('https://www.googleapis.com/customsearch/v1?q=lol+cat&cx=015345702908136904402%3A5qvmxdsto6w&searchType=image&key=AIzaSyA1XQ5N5xZsFyRc0OmoMJPhNMczYvXWEso', function (error, response, body) {
+//       if (!error && response.statusCode == 200) {
+//           // console.log(JSON.parse(body));                               // Returns body of request test
+//           var results = JSON.parse(body);
+//           // console.log((results);
+//           console.log((results.items).length);
+//           for(var i = 0; i < (results.items).length; i++) {
+//             var cseObject = {'Image URL' : results.items[i].link, 'Context' : results.items[i].link, 'Snippet' : results.items[i].snippet, 'Thumbnail' : results.items[i].image.thumbnailLink};
+//             // console.log(results.items[i]);
+//             console.log('Context: ' +results.items[i].link);
+//             console.log('Snippet: ' +results.items[i].snippet);
+//             console.log('Thumbnail: ' +results.items[i].image.thumbnailLink);
+//             console.log('Image URL: ' +results.items[i].link);
             
-            objectArr.push(cseObject);
-          }
-          // res.send(JSON.parse(body));
-          // res.send(results);
-        res.send(objectArr);
-      }
-      else {
-        res.send({'Error': 'Error establishing the connection.'});
-      }
-  })
+//             objectArr.push(cseObject);
+//           }
+//           // res.send(JSON.parse(body));
+//           // res.send(results);
+//         res.send(objectArr);
+//       }
+//       else {
+//         res.send({'Error': 'Error establishing the connection.'});
+//       }
+//   });
+  console.log()''
 });
   
 app.route('/')
